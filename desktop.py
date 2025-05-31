537364d3f071774d99f298c95e22ea505597d7fc
OrlovMatvey <deceptor27@mail.ru>
2025-05-31 15:53:14 +0300

    Добавил разделение на фреймы вместо классов. Получилось только через наследование одного фрейма другим,

--- desktop.py
+++ desktop.py
@@ -3,99 +3,58 @@ import tkinter as tk
 from logic import kv_ur
 
 
-class Labitext(tk.Tk):
-    """Класс для разделения атрибутов"""
-    def __init__(self):
-        super().__init__()
-        self.f2 = tk.Frame(self, bg="white")
-
-        self.f2.pack(side='bottom', anchor='s')
-
-        self.f1 = tk.Frame(self, bg="white")
-
-        self.f1.pack(side='top', anchor='n')
-
-        self.f3 = tk.Frame(self.f1, bg="white")
-
-        self.f3.pack(side='left', anchor='nw')
-
-        self.f4 = tk.Frame(self.f1, bg="white")
-
-        self.f4.pack(side='right', anchor='ne')
-
-        self.lab_text_1 = tk.Label(self.f2, width=80, bg='white')
-
-        self.lab_text_2 = tk.Label(self.f2, width=80, bg='white')
-
-        self.lab_text_3 = tk.Label(self.f2, width=80, bg='white')
-
-
-class Labichsl(Labitext):
-    """Класс для разделения атрибутов"""
+class Frame1(tk.Frame):
+    """Первый фрейм"""
     def __init__(self):
         super().__init__()
 
-        self.lab_otv = tk.Label(
-            self.f2, width=80, bg='white', text='Ваш ответ:'
+        self.lab_abc = tk.Label(
+            self, width=43, text='Введите значения a, b и с:',
+            bg='white', height=1
             )
 
-        self.lab_message = tk.Label(self.f2, width=80, bg='gray')
+        self.ent_a = tk.Entry(self, width=50, bg='gray')
 
-        self.lab_1 = tk.Label(self.f2, width=80, bg='gray')
+        self.ent_b = tk.Entry(self, width=50, bg='gray')
 
-        self.lab_2 = tk.Label(self.f2, width=80, bg='gray')
+        self.ent_c = tk.Entry(self, width=50, bg='gray')
 
-        self.lab_3 = tk.Label(self.f2, width=80, bg='gray')
+        self.lab_abc.pack()
+
+        self.ent_a.pack()
+
+        self.ent_b.pack()
+
+        self.ent_c.pack()
 
 
-class Kvurtkint(Labichsl):
-    """Класс для создания окна программы"""
-
+class Frame2(Frame1):
+    """Второй фрейм"""
     def __init__(self):
         super().__init__()
 
-        self.title("Решение квадратного уравнения")
+        self.lab_message = tk.Label(self, width=80, bg='gray')
 
-        self.lab_a = tk.Label(
-            self.f3, width=43, text='Введите значение a:', bg='white', height=1
-            )
+        self.lab_1 = tk.Label(self, width=80, bg='gray')
 
-        self.lab_b = tk.Label(
-            self.f3, width=43, text='Введите значение b:', bg='white', height=1
-            )
+        self.lab_2 = tk.Label(self, width=80, bg='gray')
 
-        self.lab_c = tk.Label(
-            self.f3, width=43, text='Введите значение c:', bg='white', height=1
-            )
+        self.lab_3 = tk.Label(self, width=80, bg='gray')
 
-        self.ent_a = tk.Entry(self.f4, width=43, bg='gray')
+        self.lab_text_1 = tk.Label(self, width=80, bg='white')
 
-        self.ent_b = tk.Entry(self.f4, width=43, bg='gray')
+        self.lab_text_2 = tk.Label(self, width=80, bg='white')
 
-        self.ent_c = tk.Entry(self.f4, width=43, bg='gray')
+        self.lab_text_3 = tk.Label(self, width=80, bg='white')
 
         self.but = tk.Button(
-            self.f2, width=80,
+            self, width=80,
             text='Вычислить', bg='black', fg='white', command=self.vichisl
             )
 
         self.but.bind(self, '<Button-1>', self.vichisl)
 
-        self.ent_a.pack(expand='false', fill='none', anchor='ne', pady=2)
-
-        self.lab_a.pack(expand='false', fill='none', anchor='nw', pady=1)
-
-        self.lab_b.pack(expand='false', fill='none', anchor='w', pady=1)
-
-        self.ent_b.pack(expand='false', fill='none', anchor='e', pady=2)
-
-        self.lab_c.pack(expand='false', fill='none', anchor='sw', pady=1)
-
-        self.ent_c.pack(expand='false', fill='none', anchor='se', pady=2)
-
         self.but.pack()
-
-        self.lab_otv.pack()
 
         self.lab_message.pack()
 
@@ -136,6 +95,17 @@ class Kvurtkint(Labichsl):
         elif len(otvet) == 2:
             self.lab_text_1['text'] = "Корни:"
             self.lab_1['text'] = otvet[1]
+
+
+class Kvurtkint(tk.Tk, Frame2):
+    """Класс для создания окна программы"""
+
+    def __init__(self):
+        super().__init__()
+
+        self.title("Решение квадратного уравнения")
+
+        Frame2().pack()
 
 
 if __name__ == "__main__":
